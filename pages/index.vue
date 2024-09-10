93fdaabf0162a3f4745fb4d3b4e7a9960654af44
mshrynzw <iq87io25@gmail.com>
2024-09-10 16:47:39 +0900

    Add: GSAP

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="my-element">
-    Welcome
+  <div class="box">
+    <div class="p-8 text-white bg-blue-500">
+      Welcome
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { useNuxtApp } from '#app'
+import {onMounted} from 'vue'
+import {useNuxtApp} from '#app'
 
 onMounted(() => {
-  const { $gsap } = useNuxtApp()
-  
-  $gsap.to('.my-element', {
-    rotation: 360,
-    duration: 2,
-    ease: 'bounce.out'
-  })
+  const {$gsap} = useNuxtApp()
+
+  $gsap.to(".box", {x: 200})
 })
 </script>
